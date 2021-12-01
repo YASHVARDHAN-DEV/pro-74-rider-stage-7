d23aaddcc79535e33e9c6d0e3acae4a5372ab487
YASHVARDHAN-DEV <80517622+YASHVARDHAN-DEV@users.noreply.github.com>
2021-12-01 22:38:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCZrG0BF6LrnRpWZjsPQxNGyMCLByo8HrM",
-  authDomain: "e-ride-549e4.firebaseapp.com",
-  projectId: "e-ride-549e4",
-  storageBucket: "e-ride-549e4.appspot.com",
-  messagingSenderId: "588298110700",
-  appId: "1:588298110700:web:34e3df331c6b1fc95047c9",
-  measurementId: "G-G3K7BWL5LH"
+const firebaseConfig = {
+  apiKey: "AIzaSyCU-wdwQ-S3iM6sKfFUlnwV2nITgTLpaN4",
+  authDomain: "e-rider-app-5b520.firebaseapp.com",
+  projectId: "e-rider-app-5b520",
+  storageBucket: "e-rider-app-5b520.appspot.com",
+  messagingSenderId: "883811245397",
+  appId: "1:883811245397:web:0d4ace918247feadf6a3dc"
 };
 
 firebase.initializeApp(firebaseConfig);
